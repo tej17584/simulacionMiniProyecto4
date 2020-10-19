aeaab7e7518d2fe87dc7d05ecdd7e7ebe33391b7
Alejandro Tejada <atejada@cognits.co>
2020-10-18 20:43:03 -0600

    Finish miniproyecto

--- mancalaMain.py
+++ mancalaMain.py
@@ -71,8 +71,8 @@ class Board:
         sum2 = self.board[6] + self.board[7] + self.board[8] + self.board[9] + self.board[10]
         if(sum1 == 0):
             self.finishTurns = True
-            #print("--------------------------------------------------------------------------------------")
-            #print("NO MORE TURNS FOR PLAYER 1. THE GAME HAS ENDED!")
+            print("--------------------------------------------------------------------------------------")
+            print("NO MORE TURNS FOR PLAYER 1. THE GAME HAS ENDED!")
             self.finish = True
             if(self.board[5] > self.board[11]):
                 self.winner = 'player1'
@@ -81,8 +81,8 @@ class Board:
             print()
         elif(sum2 == 0):
             self.finishTurns = True
-            #print("--------------------------------------------------------------------------------------")
-            #print("NO MORE TURNS FOR PLAYER 2. THE GAME HAS ENDED!")
+            print("--------------------------------------------------------------------------------------")
+            print("NO MORE TURNS FOR PLAYER 2. THE GAME HAS ENDED!")
             self.finish = True
             if(self.board[5] > self.board[11]):
                 self.winner = 'player1'
@@ -136,6 +136,7 @@ class Board:
             if(hops == 0):         ##if hole is empty, player most try again
                 self.tryAgainP1 = True
             else:                  ##if it is not empty, do the move
+                print("hops: "+str(hops))
                 self.board[0] = 0      ##set current hole to cero
                 i=1                    ##set next position
                 self.turnIterations(i,hops)
@@ -146,6 +147,7 @@ class Board:
             if(hops == 0):
                 self.tryAgainP1 = True
             else:
+                print("hops: "+str(hops))
                 self.board[1] = 0
                 i=2
                 self.turnIterations(i,hops)
@@ -156,6 +158,7 @@ class Board:
             if(hops == 0):
                 self.tryAgainP1 = True
             else:
+                print("hops: "+str(hops))
                 self.board[2] = 0      
                 i=3
                 self.turnIterations(i,hops)    
@@ -166,6 +169,7 @@ class Board:
             if(hops == 0):
                 self.tryAgainP1 = True
             else:                
+                print("hops: "+str(hops))
                 self.board[3] = 0      
                 i=4
                 self.turnIterations(i,hops)
@@ -176,6 +180,7 @@ class Board:
             if(hops == 0):
                 self.tryAgainP1 = True
             else:                
+                print("hops: "+str(hops))
                 self.board[4] = 0      
                 i=5
                 self.turnIterations(i,hops)
@@ -190,14 +195,15 @@ class Board:
             if(i >= len(self.board)):
                 self.board[i-len(self.board)] = self.board[i-len(self.board)]+1
             else:
-                #print("hole position: "+str(i))
-                #print("current hole old value: "+str(self.board[i]))
+                print("hole position: "+str(i))
+                print("current hole old value: "+str(self.board[i]))
                 self.board[i] = self.board[i]+1
-                #print("current hole new value: "+str(self.board[i]))
+                print("current hole new value: "+str(self.board[i]))
             i = i + 1
 
     def player2Turn(self,isRandom=False):
         '''Player 2 options for making a move in the game.'''
+        print("Calculating best move with Montecarlo algorithm")
         if(isRandom):
             choice = random.randint(1,5)
         else:
@@ -214,6 +220,7 @@ class Board:
             if(hops == 0):
                 self.tryAgainP2 = True
             else:
+                print("hops: "+str(hops))
                 self.board[6] = 0      ##set current hole to cero
                 i=7                    ##set next position
                 self.turnIterations(i,hops)
@@ -224,6 +231,7 @@ class Board:
             if(hops == 0):
                 self.tryAgainP2 = True
             else:
+                print("hops: "+str(hops))
                 self.board[7] = 0      
                 i=8
                 self.turnIterations(i,hops)
@@ -234,6 +242,7 @@ class Board:
             if(hops == 0):
                 self.tryAgainP2 = True
             else:
+                print("hops: "+str(hops))
                 self.board[8] = 0      
                 i=9
                 self.turnIterations(i,hops)    
@@ -244,6 +253,7 @@ class Board:
             if(hops == 0):
                 self.tryAgainP2 = True
             else:
+                print("hops: "+str(hops))
                 self.board[9] = 0      
                 i=10
                 self.turnIterations(i,hops)
@@ -254,88 +264,13 @@ class Board:
             if(hops == 0):
                 self.tryAgainP2 = True
             else:
+                print("hops: "+str(hops))
                 self.board[10] = 0      
                 i=11
                 self.turnIterations(i,hops)
                 self.tryAgainP2 = False
                 self.player2moves.append(choice) ##append moves made by the player to this array
         self.checkFinishTurns()          ##Check if a player has run out of turns
-
-    def player2Level(self,isRandom=False,iterations=500):
-        '''Player 2 options for making a move in the game.'''
-
-        for i in range(0,iterations):
-            firstmove, winner = self.letsPlayRandomly()
-            #print("firstmove pc player: "+str(firstmove)+" winner: "+ winner)
-            print(i)
-
-        '''if(isRandom):
-            choice = random.randint(1,5)
-        else:
-            print("\nMove: ")
-            print("1. a")
-            print("2. b")
-            print("3. c")
-            print("4. d")
-            print("5. e\n")
-            choice = read_integer()
-
-        if(choice == 1):
-            hops = self.board[6]   ##Same logic as player one, but this time we start on the 6th position
-            if(hops == 0):
-                self.tryAgainP2 = True
-            else:
-                print("hops: "+str(hops))
-                self.board[6] = 0      ##set current hole to cero
-                i=7                    ##set next position
-                self.turnIterations(i,hops)
-                self.tryAgainP2 = False
-                self.player2moves.append(choice) ##append moves made by the player to this array
-        elif(choice == 2):
-            hops = self.board[7]   ##set the hops to make based on the seeds in the hole
-            if(hops == 0):
-                self.tryAgainP2 = True
-            else:
-                print("hops: "+str(hops))
-                self.board[7] = 0      
-                i=8
-                self.turnIterations(i,hops)
-                self.tryAgainP2 = False
-                self.player2moves.append(choice) ##append moves made by the player to this array
-        elif(choice == 3):
-            hops = self.board[8]    ##set the hops to make based on the seeds in the hole
-            if(hops == 0):
-                self.tryAgainP2 = True
-            else:
-                print("hops: "+str(hops))
-                self.board[8] = 0      
-                i=9
-                self.turnIterations(i,hops)    
-                self.tryAgainP2 = False
-                self.player2moves.append(choice) ##append moves made by the player to this array
-        elif(choice == 4):
-            hops = self.board[9]    ##set the hops to make based on the seeds in the hole
-            if(hops == 0):
-                self.tryAgainP2 = True
-            else:
-                print("hops: "+str(hops))
-                self.board[9] = 0      
-                i=10
-                self.turnIterations(i,hops)
-                self.tryAgainP2 = False
-                self.player2moves.append(choice) ##append moves made by the player to this array
-        elif(choice == 5):
-            hops = self.board[10]    ##set the hops to make based on the seeds in the hole
-            if(hops == 0):
-                self.tryAgainP2 = True
-            else:
-                print("hops: "+str(hops))
-                self.board[10] = 0      
-                i=11
-                self.turnIterations(i,hops)
-                self.tryAgainP2 = False
-                self.player2moves.append(choice) ##append moves made by the player to this array
-        self.checkFinishTurns()          ##Check if a player has run out of turns'''
 
     def letsPlayRandomly(self):
         '''Computer makes a random play between players and display results.'''
@@ -359,40 +294,41 @@ class Board:
             i = i+1
 
         if(self.board[5] == self.board[11]):
-            #print("--------------------------------------------------------------------------------------")
-            #print('The is no winner!!')
-            #print('This is a tie!! :O ')
-            #print()
+            print("--------------------------------------------------------------------------------------")
+            print('The is no winner!!')
+            print('This is a tie!! :O ')
+            print()
             self.tie = True
         else:
-            #print("--------------------------------------------------------------------------------------")
-            #print('The winner is: '+ self.winner)
+            print("--------------------------------------------------------------------------------------")
+            print('The winner is: '+ self.winner)
             print()
-        #print(str(i) +' moves in total!')
-        #print('Player 1 moves: '+str(self.player1moves))
-        #print('Player 2 moves: '+str(self.player2moves))
-        #print('The final state of the board is:')
-        pcfirstmoves, winner = self.player2moves[0], self.winner
-        #self.printBoard_pretty()
+        print(str(i) +' moves in total!')
+        print('Player 1 moves: '+str(self.player1moves))
+        print('Player 2 moves: '+str(self.player2moves))
+        print('The final state of the board is:')
+        self.printBoard_pretty()
         self.resetGame()
-        return pcfirstmoves, winner
 
-    def playWithDummyComputer(self):
+    def playWithComputer(self, iterations=0):
         '''Play with Dummy computer. Computer will play with you with random moves, 
-        no intelligence or cheating.'''
+        no intelligence or cheating. And we take de ITERATIONS parameter and use it in the montecarlo algorithm'''
         i = 0
         self.printBoard_pretty()
         while(self.finish == False and self.finishTurns == False):
+            iterationstouse=iterations
             self.player1Turn()                ##player 1 makes a move
             ##if and while that force the player to choose a non empty hole       
             while(self.tryAgainP1 == True and self.finishTurns == False):
                 print('You cannot take seeds from an empty hole! Try Again!')
+                iterationstouse=iterationstouse+1
                 self.player1Turn()
 
             if(self.finishTurns == False):
                 self.player2Turn(True)            ##player 2 makes a move
             ##if and while that force the player to choose a non empty hole                 
             while(self.tryAgainP2 == True and self.finishTurns == False):
+                iterationstouse=iterationstouse+1
                 self.player2Turn(True)
 
             self.updateScore()
@@ -416,50 +352,6 @@ class Board:
         print('The final state of the board is:')
         self.printBoard_pretty()
         self.resetGame()
-
-    def advanceLevel(self):
-        '''500 iterations of cheating'''
-        i = 0
-        self.printBoard_pretty()
-        while(self.finish == False and self.finishTurns == False):
-            self.player1Turn()                ##player 1 makes a move
-            ##if and while that force the player to choose a non empty hole       
-            while(self.tryAgainP1 == True and self.finishTurns == False):
-                print('You cannot take seeds from an empty hole! Try Again!')
-                self.player1Turn()
-
-            ## 500 iterations for play 
-            if(self.finishTurns == False):
-                self.player2Turn(True)            ##player 2 makes a move
-            ##if and while that force the player to choose a non empty hole                 
-            while(self.tryAgainP2 == True and self.finishTurns == False):
-                self.player2Turn(True)
-
-            self.updateScore()
-            self.checkForWinner()
-            i = i+1
-            self.printBoard_pretty()
-
-        if(self.board[5] == self.board[11]):
-            print("--------------------------------------------------------------------------------------")
-            print('There is no winner!!')
-            print('This is a tie!! :O ')
-            print()
-            self.tie = True
-        else:
-            print("--------------------------------------------------------------------------------------")
-            print('The winner is: '+ self.winner)
-            print()
-        print(str(i) +' moves in total!')
-        print('Player 1 moves: '+str(self.player1moves))
-        print('Player 2 moves: '+str(self.player2moves))
-        print('The final state of the board is:')
-        self.printBoard_pretty()
-        self.resetGame()
-
-    def proLevel(self):
-        '''10,000 iterations of cheating'''
-
 
 Wellcome()
 
@@ -468,46 +360,32 @@ mancala = Board()
 def menu():
     exit = 0
     print("--------------------------------------------------------------------------------------")
-    print(" **********Testing**********")
-    print(" 1. My turn")
-    print(" 2. Player 2 Turn")
-    print(" ***************************")
-    print(" 3. Print board")
-    print(" 4. Noob Level")
-    print(" 5. Advance Level")
-    print(" 6. Pro Level")
-    print(" 7. Exit (Press 6 until exit)")
+    print(" 1. Play with dummy computer")
+    print(" 2. Play with advanced computer")
+    print(" 3. Play with pro computer")
+    print(" 4. Print board")
+    print(" 5. Exit (Press 6 until exit)")
     print("______________________________________________________________________________________\n")
 
     choice = read_integer()
 
     if choice == 1:
-        print("")
-        mancala.player1Turn()
-        menu()   
+        print(" ")
+        mancala.playWithComputer(iterations=0) #PASAMOS LAS 0 iteraciones
+        menu() 
     elif choice == 2:
-        print("")
-        mancala.player2Turn()
-        menu()   
+        print(" ")
+        mancala.playWithComputer(iterations=500)#PASAMOS LAS 500 iteraciones
+        menu() 
     elif choice == 3:
         print(" ")
-        mancala.printBoard_pretty()
+        mancala.playWithComputer(iterations=10000)#PASAMOS LAS 100000 iteraciones
         menu() 
     elif choice == 4:
         print(" ")
-        mancala.playWithDummyComputer()
-        menu()    
+        mancala.printBoard_pretty()
+        menu() 
     elif choice == 5:
-        print(" ")
-        #mancala.letsPlayRandomly()
-        mancala.player2Level(True,500)
-        menu() 
-    elif choice == 6:
-        print(" ")
-        #mancala.letsPlayRandomly()
-        mancala.proLevel()
-        menu() 
-    elif choice == 7:
         print(" ")
         exit = 1
         theEnd()
